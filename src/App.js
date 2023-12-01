01d396c777e5ce377790cea6e08f59c203f99ee0
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-01 12:49:59 +0100

    solved challenge

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
+}
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!</h1>
+      <p>This is an article component containing a heading and a paragraph.</p>
+      <p>
+        You can add more content to this article, such as images, lists, or code
+        snippets.
+      </p>
+      <img
+        src="https://s3.amazonaws.com/colorslive/jpg_512x512/312008-8lOV-hXS7HdRYlhV.jpg"
+        alt="Image"
+      />
+    </article>
+  );
 }
